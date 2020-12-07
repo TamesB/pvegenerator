d8ee9358cdb2589631e00f888191128aee9e0c8b
Tames <tamesboon@gmail.com>
2020-12-07 21:38:57 +0100

    fix

--- syntrus/admin.py
+++ syntrus/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from syntrus.models import FAQ, Room, CommentStatus
+from syntrus.models import FAQ, Room, CommentStatus, FrozenComments, CommentReply
 # Register your models here.
 admin.site.register(FAQ)
 admin.site.register(Room)
 admin.site.register(CommentStatus)
+admin.site.register(FrozenComments)
+admin.site.register(CommentReply)
