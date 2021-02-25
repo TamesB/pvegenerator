453dee28fab08d99b7b8c0188cdfc753ad11f420
Tames <tamesboon@gmail.com>
2021-02-25 21:06:04 +0100

    fix

--- PVE/settings.py
+++ PVE/settings.py
@@ -170,7 +170,7 @@ STATIC_URL = STATIC_HOST + '/static/'
 
 STATICFILES_DIRS = ( os.path.join('static'), )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
 GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
