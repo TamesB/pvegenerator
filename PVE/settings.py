f9ccc92e188b3aa7d37c2a6bd20ee52828162399
Tames <tamesboon@gmail.com>
2021-02-25 21:34:47 +0100

    geopy fix

--- PVE/settings.py
+++ PVE/settings.py
@@ -172,8 +172,8 @@ STATICFILES_DIRS = ( os.path.join('static'), )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
-GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
 
 # django admin site
 ADMIN_ENABLED = True
