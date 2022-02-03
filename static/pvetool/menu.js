248d387ee5424bc8619aa01e6b2880a9c9c43418
Tames <tamesboon@gmail.com>
2022-02-03 17:28:00 +0100

    menu, and header, style changes

--- static/pvetool/menu.js
+++ static/pvetool/menu.js
@@ -1,32 +1,43 @@
 var hamburger = document.getElementById("hamburger");
+var close_menu = document.getElementById("x_menu_button_1");
 var menu_left = document.getElementById("menu_left");
 var menu_links = document.getElementsByClassName("menu_link");
 var article = document.getElementById("article");
 
 hamburger.addEventListener("click", function() {
     this.classList.toggle("active")
-    toggle_menu(this, menu_links, article, menu_left);
+    toggle_menu(this, menu_links, article, menu_left, close_menu);
 
 })
 
-function toggle_menu(element, menu_links, article, menu_left) {
+close_menu.addEventListener("click", function() {
+    hamburger.classList.toggle("active")
+    toggle_menu(hamburger, menu_links, article, menu_left, close_menu);
+})
+
+function toggle_menu(element, menu_links, article, menu_left, close_menu) {
     if (element.classList.contains("active")) {
-        menu_left.style.flex = "0 0 15%;"
-        menu_left.style.minWidth = "15vw";
-        article.style.flex = "0 0 85%";
+        menu_left.style.flex = "0 0 12%;"
+        menu_left.style.minWidth = "12vw";
+        article.style.flex = "0 0 88%";
     
         for (var i = 0; i < menu_links.length; i++) {
             var link = menu_links[i];
             link.style.display = "block";
         }
+
+        close_menu.style.display = "block";
+
     } else {
-        menu_left.style.flex = "0 0 3%";
-        menu_left.style.minWidth = "3vw";
-        article.style.flex = "0 0 97%";
+        menu_left.style.flex = "0 0 2%";
+        menu_left.style.minWidth = "40px";
+        article.style.flex = "0 0 98%";
         for (var i = 0; i < menu_links.length; i++) {
             var link = menu_links[i];
             link.style.display = "none";
         }
+
+        close_menu.style.display = "none";
     };
     
 }
